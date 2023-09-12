08814b6b62d1ae3a3a8366faf97fc462824a1545
Suraj Prajapat <57035224+sprajapat770@users.noreply.github.com>
2023-09-12 07:55:42 +0530

    handle frontend requests (#15)
    * handle frontend requests
    
    * code corrected

--- public/index.php
+++ public/index.php
@@ -1,11 +1,13 @@
 <?php declare(strict_types=1);
+use Suraj\Framework\Http\Request;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // request received
-//dd("request break");
-//perform some logic
+$request = Request::createFromGlobals();
 
+//perform some logic
+dd($request);
 //send response (string of content)
 
 echo "hello world";
